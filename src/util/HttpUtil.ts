e2ce1a2f5afb7efd57c71411b41d48a7b966590a
Kwo Ding <kwo@ding.it>
2019-12-12 17:23:29 +0100

    Add proxy support in http util

--- src/util/HttpUtil.ts
+++ src/util/HttpUtil.ts
@@ -1,25 +1,32 @@
 import log from 'loglevel';
 import fetch from 'node-fetch';
 import uuid from 'uuid/v4';
+import HttpsProxyAgent from 'https-proxy-agent';
 
 const logger = log.getLogger('wiremock-rest-client');
+const proxy = process.env.WRC_HTTP_PROXY;
 
 export class HttpUtil {
     static async fetch(uri: string, options: any): Promise<any> {
         const id: string = uuid();
         const allOptions: any = { 'Content-Type': 'application/json', ...options };
 
+        if (proxy) {
+            allOptions.agent = new HttpsProxyAgent(proxy);
+            logger.debug(`[${id}] Using proxy: ${proxy}`);
+        }
+
         try {
-            logger.info(`[${id}] [INFO] Request: [${allOptions.method}] ${uri}`);
+            logger.info(`[${id}] Request: [${allOptions.method}] ${uri}`);
             if (allOptions.body !== undefined) {
-                logger.debug(`[${id}] [DEBUG] Request body: ${allOptions.body}`);
+                logger.debug(`[${id}] Request body: ${allOptions.body}`);
             }
 
             const response = await fetch(uri, allOptions);
 
             return this.parseResponse(id, response);
         } catch (error) {
-            logger.error(`[${id}] [ERROR] Error: ${error.message}`);
+            logger.error(`[${id}] Error: ${error.message}`);
 
             return process.exit(1);
         }
@@ -29,12 +36,12 @@ export class HttpUtil {
         const responseLog: string = `Response: [${response.status}] ${response.statusText}`;
 
         if (!response.ok) {
-            logger.error(`[${id}] [ERROR] ${responseLog}`);
+            logger.error(`[${id}] ${responseLog}`);
 
             return process.exit(1);
         }
 
-        logger.info(`[${id}] [INFO] ${responseLog}`);
+        logger.info(`[${id}] ${responseLog}`);
         const responseText = await response.text();
 
         try {
